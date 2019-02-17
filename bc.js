b4f1b2f9a9e4639c2d5adb1494c0da9249cdcdfe
MohanadEL-Sayed <47445920+MohanadEL-Sayed@users.noreply.github.com>
2019-02-17 16:20:04 +0200

    Add files via upload

---
+++ bc.js
@@ -0,0 +1,32 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+var prefix = "+";
+client.on("message", message => {
+
+            if (message.content.startsWith(prefix + "bc")) {
+                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' '); 
+  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
+ m.send(`${argresult}\n ${m}`);
+})
+ message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`); 
+ message.delete(); 
+};     
+});
+
+
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Desert Bot- Script By : i1Suhaib`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : i1Suhaib ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`TR3Y_WAY`,"http://twitch.tv/S-F")
+client.user.setStatus("dnd")
+});
+
+
+
+client.login("NTQ2NjU5MDU4Njc4MjM1MTM2.D0r--w.JhrR976aMKQOHjMnbf_QR7kdjx0");
